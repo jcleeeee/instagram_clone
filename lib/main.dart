a1d1787a83c52d15f665c9a9b309181117861d76
jcleeeee <101230020+jcleeeee@users.noreply.github.com>
2024-02-27 13:34:51 +0900

    회원가입 창 1차 완료 to do -없는 계정 관련 오류창 -회원가입 시 성별 데이터 받기

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
+import 'package:instagram_clone/firebase_options.dart';
+import 'package:instagram_clone/providers/auth_state.dart';
+import 'package:instagram_clone/repositories/auth_repository.dart';
+import 'package:instagram_clone/screens/signin_screen.dart';
+import 'package:instagram_clone/screens/signup_screen.dart';
+import 'package:instagram_clone/screens/splash_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:instagram_clone/providers/auth_provider.dart' as myAuthProvider;
+
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    //FirebaseAuth.instance.signOut();
+    return MultiProvider(
+      providers: [
+        Provider<AuthRepository>(
+          create: (context) => AuthRepository(
+            firebaseAuth: FirebaseAuth.instance,
+            firebaseStorage: FirebaseStorage.instance,
+            firebaseFirestore: FirebaseFirestore.instance,
+          ),
+        ),
+        StreamProvider<User?>(
+          create: (context) => FirebaseAuth.instance.authStateChanges(),
+          initialData: null,
+        ),
+        StateNotifierProvider<myAuthProvider.AuthProvider, AuthState>(
+            create: (context) => myAuthProvider.AuthProvider(),
+        ),
+
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark(),
+        home: SplashScreen(),
+      ),
+    );
+  }
+}
+
+
+
---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,76 @@
+import 'dart:typed_data';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:instagram_clone/exceptions/custom_exception.dart';
+import 'package:instagram_clone/providers/auth_state.dart';
+import 'package:instagram_clone/repositories/auth_repository.dart';
+import 'package:state_notifier/state_notifier.dart';
+
+
+class AuthProvider extends StateNotifier<AuthState> with LocatorMixin {
+  AuthProvider() : super(AuthState.init());
+
+  @override
+  void update(Locator watch){
+    final user = watch<User?>();
+
+    // 회원가입만 한 상태에서 오류 수정
+    if (user != null && !user.emailVerified){
+      return;
+    }
+
+    // 미인증에서 미인증으로 화면 다시 송출 오류
+    if (user == null && state.authStatus == AuthStatus.unauthenticated){
+      return;
+    }
+
+    if (user != null){
+      state = state.copyWith(
+        authStatus: AuthStatus.authenticated,
+      );
+    } else {
+      state = state.copyWith(
+          authStatus: AuthStatus.unauthenticated,
+      );
+    }
+  }
+
+  Future<void> signOut() async {
+    await read<AuthRepository>().signOut();
+
+  }
+
+  Future<void> signUp({
+    required String email,
+    required String name,
+    required String password,
+    required Uint8List? profileImage
+    }) async{
+      try {
+        await read<AuthRepository>().signUp(
+            email: email,
+            name: name,
+            password: password,
+            profileImage: profileImage
+        );
+      } on CustomException catch (_) {
+        rethrow;
+      }
+  }
+
+  Future<void> signIn({
+    required String email,
+    required String password,
+  }) async{
+    try {
+      await read<AuthRepository>().signIn(
+          email: email,
+          password: password
+      );
+
+    } on CustomException catch(_) {
+      rethrow;
+    }
+
+  }
+}
